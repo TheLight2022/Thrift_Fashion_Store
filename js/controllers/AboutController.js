261bb64981614363068d398818d99c83502e769b
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-08-10 11:26:16 +0700

    angular

---
+++ js/controllers/AboutController.js
@@ -0,0 +1,3 @@
+app.controller('AboutController',['$scope', function($scope) {
+    $scope.message = "AboutController"
+}]);
---
+++ js/controllers/BlogController.js
@@ -0,0 +1,3 @@
+app.controller('BlogController', ['$scope', function($scope) {
+    $scope.message = "BlogController"
+}]);
