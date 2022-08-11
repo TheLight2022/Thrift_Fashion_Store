747bdfe64a105b5494711cd3b45649d21f47f473
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-08-11 13:26:08 +0700

    quý

--- js/controllers/HeaderController.js
+++ js/controllers/HeaderController.js
@@ -1,7 +1,10 @@
 app.controller('myHeader', ['$scope','dataProducts','dataCategories','$routeParams',function($scope,dataProducts,dataCategories,$routeParams) {
     $scope.currentPage = $routeParams.id
     $scope.search = $scope.currentPage
-
+    $scope.noProduct = false
+    $scope.resetValue = function(){
+        $scope.search = ""
+    }
     dataProducts.success(function(data) {
         $scope.products = data;
         $scope.dataProducts = data
@@ -22,35 +25,27 @@ app.controller('myHeader', ['$scope','dataProducts','dataCategories','$routePara
     
     
     $scope.filterProducts = function(){
-          // Lọc từ khóa
-          key = $scope.search
-           
-        if(key){
-            $scope.search = $scope.search.replace('\  \g',"").trim()
-            re = new RegExp(key, "gi")
-            filter = $scope.products.filter(el=>{
-                isTrue = el.name.match(re) ||
-                el.seller.name.match(re) ||
-                el.brand.match(re) ||
+        if(!$scope.search){
+            $scope.dataProducts = $scope.products
+        }else{
+            key = $scope.search
+            re = new RegExp(key,"gi")
+
+            $scope.dataProducts = $scope.products.filter(el=>{
+                isTrue = el.name.match(re)||
                 el.category.match(re) 
                 return isTrue
             })
-            $scope.dataProducts = filter
-        }else{
-            $scope.dataProducts = $scope.products
         }
 
-        //Lọc category
-        // $scope.dataProducts = $scope.dataProducts.filter(el=>{
-        //     return $scope.categorys.find(item=>item.name == el.category)
-        // })
+        
 
         if($scope.dataProducts.length > 9){
-            $scope.dataPage = Math.ceil($scope.dataProducts.length / 9)
+            $scope.dataPage = Math.ceil($scope.dataProducts.length/9)
             $scope.noProduct = false
         }else{
             $scope.dataPage = false
-            if($scope.dataProducts.length==0){
+            if($scope.dataProducts.length == 0){
                 $scope.noProduct = true
             }
         }
