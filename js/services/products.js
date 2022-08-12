9e9d09c5177dcffbad0f741c02a7109a1467c314
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-08-12 15:20:23 +0700

    update

--- js/services/products.js
+++ js/services/products.js
@@ -1,7 +1,6 @@
 app.factory('dataProducts', ['$http', function($http) {
     return $http.get('../../json/main.json')
       .success(function(data) {
-        console.log(data);
         return data;
       })
       .error(function(err) {
