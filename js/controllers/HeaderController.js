75a11e45e0b162ae91e2cdc037c73de68ef089dd
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-08-13 10:06:36 +0700

    update

--- js/controllers/HeaderController.js
+++ js/controllers/HeaderController.js
@@ -1,10 +1,8 @@
 app.controller('myHeader', ['$scope','dataProducts','dataCategories',function($scope,dataProducts,dataCategories) {
     if(localStorage.getItem('visit')){
         $scope.visit = JSON.parse(localStorage.getItem('visit'))
-        $scope.visit.visit =+1
-
     }else{
-        localStorage.setItem('visit',JSON.stringify({visit:1}))
+        localStorage.setItem('visit',JSON.stringify({visit:40}))
     }
 
 
